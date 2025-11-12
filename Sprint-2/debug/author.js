9b84a4eefe61d8b7e95fba9cb88efa43301833c8
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-11-12 13:35:28 +0100

    Update author.js
    files modified

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -11,6 +11,11 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+/*for (const value of author) {
+  console.log(value);
+}*/
+
+for (const value of Object.values(author)) {
   console.log(value);
 }
+
