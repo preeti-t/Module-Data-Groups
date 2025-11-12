3b1edc021120f746cab9d4a6ca01d6d68303d180
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-11-12 13:23:05 +0100

    Update address.js
    files modified

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -12,4 +12,8 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+/*console.log(`My house number is ${address[0]}`);*/
+
+console.log(`My house number is ${address.houseNumber}`);
+
+
