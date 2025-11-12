2f1fc216e9375e8772d210fac87fabeba3047828
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-11-12 09:44:51 +0100

    Update max.js
    code completed

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,8 @@
 function findMax(elements) {
+   const numbers = elements.filter(item => typeof item === "number" && !Number.isNaN(item));
+   
+   if (numbers.length === 0) return -Infinity;
+      return Math.max(...numbers); 
 }
 
 module.exports = findMax;
