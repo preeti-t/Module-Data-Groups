e8cd3de3b4fe0270d7dcf79e93f0b9433078bd0a
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-11-20 23:43:04 +0100

    Update tally.js
    tally code created

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,9 @@
-function tally() {}
+function tally(arr) {
+  const result = {};
+  for (const item of arr) {
+    result[item] = (result[item] || 0) + 1;
+  }
+  return result;
+}
 
 module.exports = tally;
