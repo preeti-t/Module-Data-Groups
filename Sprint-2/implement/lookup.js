b1d213249446f9db5e03821fdd1f4ab513145a2f
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-11-20 22:09:07 +0100

    Update lookup.js
    code completed

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,9 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+  if (!Array.isArray(pairs)) {
+    throw new Error("Input must be an array of pairs");
+  }
+
+  return Object.fromEntries(pairs);
 }
 
 module.exports = createLookup;
