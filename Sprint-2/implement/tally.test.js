73e6d0811437addd9c3926b0d4eb5ff05b394b01
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-11-20 23:55:59 +0100

    Update tally.test.js
    added test cases

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -20,15 +20,35 @@ const tally = require("./tally.js");
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
 
+test("returns an object with counts for each unique item", () => {
+  expect(tally(["a", "a", "b", "c"])).toEqual({ a: 2, b: 1, c: 1 });
+  expect(tally(["cow", "goat", "cow", "fox"])).toEqual({
+    cow: 2,goat: 1,fox: 1,
+  });
+});
+
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+
+test("tally on an empty array returns an empty object", () => {
+  expect(tally([])).toEqual({});
+});
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
 
+test("tally returns counts for each unique item", () => {
+  expect(tally(["a", "a", "b", "c"])).toEqual({ a: 2, b: 1, c: 1 });
+  expect(tally(["x", "x", "x"])).toEqual({ x: 3 });
+});
+
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+
+test("throws an error for invalid input", () => {
+  expect(() => tally("not an array")).toThrow("Input must be an array");
+  expect(() => tally(123)).toThrow("Input must be an array");
+});
