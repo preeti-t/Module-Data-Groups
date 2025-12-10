7e4a0eb2a995db0439c6f95219a483fa2ce4e024
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-12-10 10:20:19 +0100

    Update quotes.js
    fixed errors

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -1,3 +1,16 @@
+window.onload = function () {
+function displayRandomQuote () {
+  const randomQuote = pickFromArray(quotes);
+
+  document.getElementById("quote").innerText = randomQuote.quote;
+  document.getElementById("author").innerText = " - " + randomQuote.author;
+}
+displayRandomQuote();
+document
+  .getElementById("new-quote")
+  .addEventListener("click", displayRandomQuote);
+};
+
 // DO NOT EDIT BELOW HERE
 
 // pickFromArray is a function which will return one item, at
