572861455e7488b64376a818b36e7be1e4876c36
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-01-15 13:37:45 +0100

    Update max.test.js
    updated to avoid confusion between two findMax(), one in the main file and another in the test file.

--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -9,64 +9,70 @@ You should implement this function in max.js, and add tests for it in this file.
 
 We have set things up already so that this file can see your function from the other file.
 */
-function findMax(arr) {
-  
-  const numbers = arr.filter(x => typeof x === "number"); // only keep numbers
 
-  if (numbers.length === 0) return -Infinity; 
-  // if there are no numbers, return -Infinity
-
-  // find the maximum value
-  return Math.max(...numbers);
-}
-
-module.exports = findMax;
-
+const findMax = require("./max.js");
 
 // Given an empty array
 // When passed to the max function
 // Then it should return -Infinity
 // Delete this test.todo and replace it with a test.
-test.todo("given an empty array, returns -Infinity");
+test("given an empty array, returns -Infinity", () => {
+    const elements = [];
+    const currentOutput = findMax(elements);
+    expect(currentOutput).toEqual(-Infinity);
+});
 
 // Given an array with one number
 // When passed to the max function
 // Then it should return that number
-test("returns that number if array has one number", () => {
-  expect(findMax([7])).toBe(7);
+test("given an array with one number, returns that number", () => {
+    const elements = [5];
+    const currentOutput = findMax(elements);
+    expect(currentOutput).toEqual(5);
 });
 
 // Given an array with both positive and negative numbers
 // When passed to the max function
 // Then it should return the largest number overall
-test("returns the largest number overall", () => {
-  expect(findMax([-5, 0, 20, 10, -1])).toBe(20);
+test("given an array with both positive and negative numbers, returns the largest number overall", () => {
+    const elements = [5, -1, 10, -81];
+    const currentOutput = findMax(elements);
+    expect(currentOutput).toEqual(10);
 });
 
 // Given an array with just negative numbers
 // When passed to the max function
 // Then it should return the closest one to zero
-test("returns the closest to zero", () => {
-  expect(findMax([-10, -3, -7])).toBe(-3);
+test("given an array with just negative numbers, returns the closest one to zero", () => {
+    const elements = [-5, -1, -10, -81];
+    const currentOutput = findMax(elements);
+    expect(currentOutput).toEqual(-1);
 });
 
 // Given an array with decimal numbers
 // When passed to the max function
 // Then it should return the largest decimal number
-test("returns the largest decimal number", () => {
-  expect(findMax([1.2, 3.5, 2.8])).toBe(3.5);
+test("given an array with decimal numbers, returns the largest decimal number", () => {
+    const elements = [5.3, 1.5, -10.01, 81.4]; //tried with negative decimals too
+    const currentOutput = findMax(elements);
+    expect(currentOutput).toEqual(81.4);
 });
 
 // Given an array with non-number values
 // When passed to the max function
 // Then it should return the max and ignore non-numeric values
-test("ignores non-numeric values and returns max number", () => {
-  expect(findMax(["hey", 10, "hi", 60, 10])).toBe(60);
+test("given an array with non-number values, returns the max and ignore non-numeric values", () => {
+    const elements = [5, "g", 10, "&"];
+    const currentOutput = findMax(elements);
+    expect(currentOutput).toEqual(10);
 });
 
 // Given an array with only non-number values
 // When passed to the max function
 // Then it should return the least surprising value given how it behaves for all other inputs
-test("returns -Infinity when all values are non-numeric", () => {
-  expect(findMax(["a", "b", "c"])).toBe(-Infinity);
+test("given an array with only non-numbers, returns -Infinity", () => {
+    const elements = ["é", "g", "M", "&"];
+    const currentOutput = findMax(elements);
+    expect(currentOutput).toEqual(-Infinity);
+});
 });
