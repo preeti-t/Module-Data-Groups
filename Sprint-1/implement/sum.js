4562191b4241061e82c17f2865bfec01b77fd6a1
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-01-15 13:39:53 +0100

    Update sum.js
    updated as per requirement

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,13 +1,20 @@
-function sum(arr) {
-  let total = 0;
-
-  for (const item of arr) {
-    if (typeof item === "number" && !Number.isNaN(item)) {
-      total += item;
+function sum(elements) {
+    if (elements.length === 0) {
+        //If array of elements is empty
+        return 0;
     }
-  }
-
-  return total;
+    const numbers = []; //Empty array to add only numerical items
+    for (let i = 0; i < elements.length; i++) {
+        //loop
+        if (typeof elements[i] === "number") {
+            numbers.push(elements[i]); //When a number is found it is added to the end of the numbers array
+        }
+    }
+    let total = 0; //Start of a loop to add every numerical item to the previous
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    return total; //Sum of all items in the numbers array
 }
 
 module.exports = sum;
